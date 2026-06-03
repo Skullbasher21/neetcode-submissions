88a1884cfa72f1cbcedb432bd45a1603740db7c3
Skullbasher21 <t2016du@gmail.com>
2026-06-03 14:34:11 -0400

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        aslist = list(s1)
+        for i in range(0, len(s2) - len(s1)+1):
+            word = s2[i:i+len(s1)]
+            wl = list(word)
+            for j in range(len(aslist)):
+                if aslist[j] not in wl or aslist[j] not in word:
+                    break
+                if j == len(aslist) - 1 and aslist[j] in word:
+                    print(word)
+                    return True
+                
+                wl.remove(aslist[j])
+        return False
+                
